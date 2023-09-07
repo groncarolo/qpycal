77a531fee8a8eb2470b941da59240f8c4ade9466
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-07 13:26:22 +0200

    main: print result matrix
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qpycalc.py
+++ qpycalc.py
@@ -1,11 +1,13 @@
 import argparse
 import logging
+import sys
+
+import numpy as np
 
 from qjson import from_json
 from qparser import parse_and_calculate
 from qutils import print_cartesian_coordinates, complex_2_cartesian_coordinates
 from qutils import complex_2_spherical_coordinates, print_spherical_coordinates
-
 
 
 def main():
@@ -15,12 +17,15 @@ def main():
     arg_parser.add_argument("-j", action='store_true')
     args, leftovers = arg_parser.parse_known_args()
 
+    print("left -> bottom right -> top")
     while True:
         try:
             if args.j is True:
                 j_str = input('qpycal > ')
                 circuit, result = from_json(j_str)
             else:
+                # left bottom
+                # right top
                 circuit = input('qpycal > ')
         except EOFError:
             print("")
@@ -28,7 +33,7 @@ def main():
 
         if not circuit:
             continue
-        ret = parse_and_calculate(circuit)
+        ret, in_len = parse_and_calculate(circuit)
 
         if len(ret) == 2:
             ret_r_ph_th = complex_2_spherical_coordinates(ret)
@@ -36,8 +41,18 @@ def main():
             print_spherical_coordinates(ret_r_ph_th)
             print_cartesian_coordinates(ret_xyz)
         else:
-            print(ret)
-            #print(ret.reshape((4, 2)))
+            np.set_printoptions(linewidth=sys.maxsize)
+            np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+            print(ret.shape[0])
+            if (in_len % 2) == 0:
+                a = int(np.sqrt(ret.shape[0]))
+                b = int(np.sqrt(ret.shape[0]))
+            else:
+                b = int(np.sqrt(ret.shape[0]*2))
+                a = b //2
+
+            print(ret.reshape(b, a))
 
 
 if __name__ == "__main__":
