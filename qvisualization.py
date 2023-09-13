b45332a66c1a80c46f89c7bfc6e8c11d6c584b29
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-13 19:47:56 +0200

    [29] add history with readline
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qvisualization.py
+++ qvisualization.py
@@ -1,3 +1,10 @@
+import sys
+import numpy as np
+
+from qutils import complex_2_spherical_coordinates, complex_2_cartesian_coordinates, print_spherical_coordinates, \
+    print_cartesian_coordinates
+
+
 def display_circuit(states, gates):
     for i in reversed(range(len(states))):
         print(states[i].label, end="")
@@ -6,3 +13,24 @@ def display_circuit(states, gates):
             print(gates[j][i].label, end="")
             print(" ", end="")
         print("")
+
+
+def display_result(ret, in_len):
+    if len(ret) == 2:
+        ret_r_ph_th = complex_2_spherical_coordinates(ret)
+        ret_xyz = complex_2_cartesian_coordinates(ret)
+        print_spherical_coordinates(ret_r_ph_th)
+        print_cartesian_coordinates(ret_xyz)
+    else:
+        np.set_printoptions(linewidth=sys.maxsize)
+        np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+        print(ret.shape[0])
+        if (in_len % 2) == 0:
+            a = int(np.sqrt(ret.shape[0]))
+            b = int(np.sqrt(ret.shape[0]))
+        else:
+            b = int(np.sqrt(ret.shape[0] * 2))
+            a = b // 2
+
+        print(ret.reshape(b, a))
