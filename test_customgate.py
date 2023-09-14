942dca69eb8997998ecd54ff1fa991c6e99a6f37
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-14 23:58:55 +0200

    [34] test custom gates
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ test_customgate.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+from qparser import parse_and_calculate
+
+testdata_calculate = [
+    ('''|0>*|1>*|+>*|1> G(sum)*I X*Y*Z*X X*I*I*I''', np.array([[0. + 0.j, 0. + 0.j, 0. + 0.70710678j, 0. + 0.j],
+                                                               [0. + 0.j, 0. + 0.j, 0. + 0.j, 0. + 0.j],
+                                                               [0. - 0.70710678j, 0. + 0.j, 0. + 0.j, 0. + 0.j],
+                                                               [0. + 0.j, 0. + 0.j, 0. + 0.j, 0. + 0.j]])),
+]
+
+
+@pytest.mark.parametrize("in_str,res", testdata_calculate)
+def test_calculate(in_str, res):
+    result, in_len = parse_and_calculate(in_str)
+    assert np.allclose(result, res.flatten())
