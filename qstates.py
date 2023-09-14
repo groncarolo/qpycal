33631185eec45f897243e76f975a6c3399108456
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-14 22:58:55 +0200

    add unitary no adjacent gate
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qstates.py
+++ qstates.py
@@ -1,4 +1,5 @@
 import numpy as np
+import logging
 
 
 class State:
@@ -15,14 +16,17 @@ def is_normalized(state):
 
 
 class State0:
+    label = "|0>"
     state = np.array([1., 0.])
 
 
 class State1:
+    label = "|1>"
     state = np.array([0., 1.])
 
 
 class StatePlus:
+    label = "|+>"
     state = np.array([1. / np.sqrt(2.), 1. / np.sqrt(2.)])
 
 
@@ -39,3 +43,39 @@ class StateI:
 class StateMinusI:
     label = "|-i>"
     state = np.array([1. / np.sqrt(2.), -1.j / np.sqrt(2.)])
+
+
+# i control j target
+# U
+# X
+def get_U_01(state_shape, gate):
+    zero = np.array([1., 0.])
+    one = np.array([0., 1.])
+    ide2 = np.identity(2)
+    idex = np.identity(state_shape // 4)
+    out = np.outer(zero, np.conjugate(zero).T)
+    a = np.kron(np.kron(ide2, idex), out)
+    out2 = np.outer(one, np.conjugate(one).T)
+    b = np.kron(np.kron(gate, idex), out2)
+    g0 = a + b
+    g0.shape
+    return g0
+
+
+# i control j target
+# U
+# C
+def get_U_10(state_shape, gate):
+    zero = np.array([1., 0.])
+    one = np.array([0., 1.])
+    ide2 = np.identity(2)
+    idex = np.identity(state_shape // 4)
+    out = np.outer(zero, np.conjugate(zero).T)
+    a = np.kron(np.kron(out, ide2), idex)
+    logging.info(a.shape)
+    out2 = np.outer(one, np.conjugate(one).T)
+    b = np.kron(np.kron(out2, idex), gate)
+    logging.info(b.shape)
+    g0 = a + b
+    g0.shape
+    return g0
