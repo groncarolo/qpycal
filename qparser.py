12e9210b4fe1cb686a2c87692f936ee3d6499db0
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-14 22:58:55 +0200

    add unitary no adjacent gate to parser
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qparser.py
+++ qparser.py
@@ -1,29 +1,94 @@
 import logging
 
 from ply import yacc
+import numpy as np
 
-from qgates import apply_gate, Gate
-from qstates import is_normalized, State
+from qgates import apply_gate, Gate, Ctrl, ACtrl, XGate
+from qstates import is_normalized, State, get_U_10, get_U_01
 from qutils import tensor_prod
 from qlexer import tokens
+from functools import reduce
+
+
+def display_circuit(states, gates):
+    for i in reversed(range(len(states))):
+        print(states[i].label, end="")
+        print(" ", end="")
+        for j in reversed(range(len(gates))):
+            print(gates[j][i].label, end="")
+            print(" ", end="")
+        print("")
+
 
 def p_circuit(p):
     '''circuit : states gates'''
     logging.info("p_circuit")
+    display_circuit(p[1], p[2])
 
-    if not is_normalized(p[1]):
+    if len(p[1]) > 1:
+        state = reduce(lambda x, y: State(tensor_prod(x.state, y.state)), p[1])
+    else:
+        state = p[1][0]
+
+    if not is_normalized(state):
         raise
-    ret = p[1].state
-    for g in p[2]:
+    ret = state.state
+    for col in p[2]:
         logging.info("Multiplying:")
         logging.info(ret)
         logging.info("*")
-        logging.info(g.gate)
-        ret = apply_gate(g, ret)
+
+        for g in col:
+            logging.info(g.gate)
+
+        if len(col) > 1:
+            # look for a control
+            ctrls = [i for i, e in enumerate(col) if type(e) == type(Ctrl())]
+            logging.info("ctrls")
+            logging.info(ctrls)
+            actrls = [i for i, e in enumerate(col) if type(e) == type(ACtrl())]
+            logging.info("actrls")
+            logging.info(actrls)
+            nots = [i for i, e in enumerate(col) if type(e) == type(XGate())]
+            logging.info("nots")
+            logging.info(nots)
+
+            if len(ctrls) > 0:
+                # remove from list between indexes
+                if ctrls[0] < nots[0]:
+                    start_index=ctrls[0]
+                    end_index=nots[0]+1
+                    op=get_U_10
+                else:
+                    start_index = nots[0]
+                    end_index = ctrls[0]+1
+                    op = get_U_01
+                del col[start_index:end_index]
+
+                logging.info("now col is:")
+                logging.info(col)
+                # if there is nothing else in the column
+                # this is the final gate
+                g = Gate(op(2**(end_index-start_index), XGate.gate))
+                if len(col) == 0:
+                    complete = g
+                else:
+                    # we need to insert the new gate in the correct position
+                    col.insert(start_index, g)
+                    complete = reduce(lambda x, y: Gate(tensor_prod(x.gate, y.gate)), col)
+            else:
+                complete = reduce(lambda x, y: Gate(tensor_prod(x.gate, y.gate)), col)
+        else:
+            # this is one bit case
+            complete = col[0]
+        logging.info(complete.gate)
+
+        ret = apply_gate(complete, ret)
         logging.info("=")
         logging.info(ret)
+        logging.info("^^^^^^^^^^^")
 
-    p[0] = ret
+    p[0] = ret,len(p[1])
 
 
 def p_factor(p):
@@ -65,9 +130,12 @@ def p_states(p):
     | comp_state'''
     logging.info("p_states")
     if len(p) == 2:
-        p[0] = p[1]
+        logging.info(">>len 2")
+        p[0] = [p[1]]
     elif len(p) == 4:
-        p[0] = State(tensor_prod(p[1].state, p[3].state))
+        logging.info(">>len 4")
+        p[1].append(p[3])
+        p[0] = p[1]
 
 
 def p_comp_states(p):
@@ -107,17 +175,20 @@ def p_gates(p):
               | gates TIMES gate
               | gate'''
     logging.info("p_gates")
+
     if len(p) == 2:
         logging.info("p_gates: first gate")
-        p[0] = [p[1]]
+        # create a list
+        p[0] = [[p[1]]]
     elif len(p) == 4:
+        logging.info("p_gates: len4")
         last = p[1][len(p[1]) - 1]
-        r = Gate(tensor_prod(last.gate, p[3].gate))
-        p[1][len(p[1]) - 1] = r
+        last.append(p[3])
         p[0] = p[1]
     else:
         logging.info("p_gates: append")
-        p[1].append(p[2])
+        last = p[1]
+        last.append([p[2]])
         p[0] = p[1]
 
 
@@ -139,7 +210,7 @@ def p_gate(p):
 
 # Error rule for syntax errors
 def p_error(p):
-    logging.error("Syntax error in input! %s" , str(p))
+    logging.error("Syntax error in input! %s", str(p))
     raise RuntimeError
 
 
