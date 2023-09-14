71e94e832ba29dde09c395de072989bc7f15f20c
Guido Roncarolo <guido.roncarolo@gmail.com>
2023-09-14 19:58:55 +0200

    [32] print also abs square and phase
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qvisualization.py
+++ qvisualization.py
@@ -1,8 +1,7 @@
 import sys
 import numpy as np
 
-from qutils import complex_2_spherical_coordinates, complex_2_cartesian_coordinates, print_spherical_coordinates, \
-    print_cartesian_coordinates, complex_2_polar_coordinates
+from qutils import complex_2_polar_coordinates, rad_2_deg
 
 
 def display_circuit(states, gates):
@@ -16,20 +15,21 @@ def display_circuit(states, gates):
 
 
 def display_result(ret, in_len):
-    if len(ret) == 2:
-        ret_r_ph_th = complex_2_spherical_coordinates(ret)
-        ret_xyz = complex_2_cartesian_coordinates(ret)
-        print_spherical_coordinates(ret_r_ph_th)
-        print_cartesian_coordinates(ret_xyz)
+    np.set_printoptions(linewidth=sys.maxsize)
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+    if (in_len % 2) == 0:
+        a = int(np.sqrt(ret.shape[0]))
+        b = int(np.sqrt(ret.shape[0]))
     else:
-        np.set_printoptions(linewidth=sys.maxsize)
-        np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+        b = int(np.sqrt(ret.shape[0] * 2))
+        a = b // 2
 
-        if (in_len % 2) == 0:
-            a = int(np.sqrt(ret.shape[0]))
-            b = int(np.sqrt(ret.shape[0]))
-        else:
-            b = int(np.sqrt(ret.shape[0] * 2))
-            a = b // 2
+    print("values:")
+    print(ret.reshape(b, a))
 
-        print(ret.reshape(b, a))
+    abs, angles = complex_2_polar_coordinates(ret)
+    print("mag^2:")
+    print(np.square(abs).reshape(b, a))
+    print("phase:")
+    print(rad_2_deg(angles).reshape(b, a))
