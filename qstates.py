911886a854df1034273a4858badf45e311f1f3db
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-04-22 16:18:21 +0200

    [64] make API more pythonic
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- qstates.py
+++ qstates.py
@@ -1,84 +1,106 @@
+"""
+>>> s = qstate_0()
+>>> s
+QState(state=array([1.+0.j, 0.+0.j]), label='|0>', name='')
+>>> s.is_normalized()
+True
+>>> s1 = qstate_1()
+>>> s1 == s
+False
+>>> zero = qstate_0()
+>>> one = qstate_1()
+>>> type(np.conjugate(zero))
+<class 'numpy.ndarray'>
+>>> E00 = np.outer(zero, np.conjugate(zero).T)
+>>> E00
+array([[1.+0.j, 0.+0.j],
+       [0.+0.j, 0.+0.j]])
+>>> type(E00)
+<class 'numpy.ndarray'>
+>>> E01 = np.outer(zero, np.conjugate(one).T)
+>>> E01
+array([[0.+0.j, 1.+0.j],
+       [0.+0.j, 0.+0.j]])
+>>> E10 = np.outer(one, np.conjugate(zero).T)
+>>> E10
+array([[0.+0.j, 0.+0.j],
+       [1.+0.j, 0.+0.j]])
+>>> E11 = np.outer(one, np.conjugate(one).T)
+>>> E11
+array([[0.+0.j, 0.+0.j],
+       [0.+0.j, 1.+0.j]])
+"""
+from dataclasses import dataclass
 import numpy as np
 
-
-class State:
-    '''
+@dataclass(eq=False)
+class QState:
+    """
     Class Representing a quantum state
-    '''
+    """
+    state: np.ndarray
+    label: str = str()
+    name: str = str()
 
-    def __init__(self, state, label="", name=""):
-        self.state = state
-        self.label = label
-        self.name = name
+    def __array__(self):
+        return self.state
 
+    #@property
+    # def label(self):
+    #     return self._label
 
-def is_state_normalized(state):
-    '''
-    returns if the state is normalized
-    meaning |a|^2+|b|^2 = 1
-    :param state: state to be checked
-    :return: if the state is normalized
-    '''
-    r = np.conjugate(state.state)
-    np.multiply(state.state, r, out=r)
-    return np.isclose(np.sum(r), 1.0)
+    # @property
+    # def name(self):
+    #     return self._name
+    # @property
+    # def state(self):
+    #     return self._state
 
+    def __len__(self):
+        return len(self.state)
+    def __iter__(self):
+        return iter(self.state)
 
-class State0(State):
-    '''
-    State |0>
-    '''
+    def __eq__(self, other):
+        return (self.state.shape == other.state.shape and
+                (self.state == other.state).all())
 
-    def __init__(self, name=""):
-        super().__init__(np.array([1., 0.], dtype=complex),
-                         "|0>", name)
-
-
-class State1(State):
-    '''
-    State |1>
-    '''
-
-    def __init__(self, name=""):
-        super().__init__(np.array([0., 1.], dtype=complex),
-                         "|1>", name)
-
-
-class StatePlus(State):
-    '''
-    State |+>
-    '''
-
-    def __init__(self, name=""):
-        super().__init__(np.array([1. / np.sqrt(2.), 1. / np.sqrt(2.)], dtype=complex),
-                         "|+>", name)
+    def is_normalized(self):
+        """
+        returns if the state is normalized
+        meaning |a|^2+|b|^2 = 1
+        :return: if the state is normalized
+        """
+        r = np.conjugate(self)
+        np.multiply(self, r, out=r)
+        return np.isclose(np.sum(r), 1.0)
 
 
-class StateMinus(State):
-    '''
-    State |->
-    '''
-
-    def __init__(self, name=""):
-        super().__init__(np.array([1. / np.sqrt(2.), -1. / np.sqrt(2.)], dtype=complex),
-                         "|->", name)
+def qstate_0() -> 'QState':
+    """ QState |0> """
+    return QState(np.array([1., 0.], dtype=complex), "|0>")
 
 
-class StateI(State):
-    '''
-    State |i>
-    '''
-
-    def __init__(self, name=""):
-        super().__init__(np.array([1. / np.sqrt(2.), 1.j / np.sqrt(2.)], dtype=complex),
-                         "|i>", name)
+def qstate_1() -> 'QState':
+    """ QState |1> """
+    return QState(np.array([0., 1.], dtype=complex), "|1>")
 
 
-class StateMinusI(State):
-    '''
-    State |-i>
-    '''
+def qstate_plus() -> 'QState':
+    """ QState |+> """
+    return QState(np.array([1. / np.sqrt(2.), 1. / np.sqrt(2.)], dtype=complex), "|+>")
 
-    def __init__(self, name=""):
-        super().__init__(np.array([1. / np.sqrt(2.), -1.j / np.sqrt(2.)], dtype=complex),
-                         "|-i>", name)
+
+def qstate_minus() -> 'QState':
+    """ QState |-> """
+    return QState(np.array([1. / np.sqrt(2.), -1. / np.sqrt(2.)], dtype=complex), "|->")
+
+
+def qstate_i() -> 'QState':
+    """ QState |i> """
+    return QState(np.array([1. / np.sqrt(2.), 1.j / np.sqrt(2.)], dtype=complex), "|i>")
+
+
+def qstate_minus_i() -> 'QState':
+    """ QState |-i> """
+    return QState(np.array([1. / np.sqrt(2.), -1.j / np.sqrt(2.)], dtype=complex), "|-i>")
--- qunitary.py
+++ qunitary.py
@@ -1,25 +1,24 @@
 import numpy as np
+from qstates import qstate_0, qstate_1
 
-from qstates import State0, State1
-
-zero = State0().state
-one = State1().state
+zero = qstate_0()
+one =  qstate_1()
 E00 = np.outer(zero, np.conjugate(zero).T)
 E01 = np.outer(zero, np.conjugate(one).T)
-E10 = np.outer(one, np.conjugate(zero).T)
-E11 = np.outer(one, np.conjugate(one).T)
+E10 = np.outer(one,  np.conjugate(zero).T)
+E11 = np.outer(one,  np.conjugate(one).T)
 ide2 = np.identity(2, dtype=complex)
 
 
-def get_cnot_ctrl_01(how_many_bits, ctrls, gate):
-    '''
+def get_cnot_ctrl_01(n, ctrls, gate):
+    """
     get a CNOT like
     # C
     # .
     # .
     # X
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     idex = np.identity(state_shape // 4)
     out = np.outer(zero, np.conjugate(zero).T)
     a = np.kron(np.kron(out, ide2), idex)
@@ -30,13 +29,13 @@ def get_cnot_ctrl_01(how_many_bits, ctrls, gate):
 
 
 def get_cnot_actrl_01(state_shape):
-    '''
+    """
     get a CNOT like
     # A
     # .
     # .
     # X
-    '''
+    """
     idex = np.identity(state_shape // 4, dtype=complex)
     gnot = np.array([[0., 1.], [1., 0.]], dtype=complex)  # not
     a = np.kron(E00, np.kron(idex, gnot))
@@ -46,8 +45,8 @@ def get_cnot_actrl_01(state_shape):
 
 # U
 # C
-def get_unitary_gate_01(how_many_bits, ctrls, gate):
-    '''
+def get_unitary_gate_01(n, ctrls, gate):
+    """
     get a TOFFOLI like
     # C
     # .
@@ -56,12 +55,12 @@ def get_unitary_gate_01(how_many_bits, ctrls, gate):
     # .
     # .
     # X
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     ret = np.zeros((state_shape, state_shape), dtype=complex)
 
-    for i in range(2 ** (how_many_bits - 2)):  # 8
-        binary = "{0:>0{1}b}".format(i, how_many_bits - 2)
+    for i in range(2 ** (n - 2)):  # 8
+        binary = "{0:>0{1}b}".format(i, n - 2)
         print(binary)
 
         if i in ctrls:
@@ -83,8 +82,8 @@ def get_unitary_gate_01(how_many_bits, ctrls, gate):
     return ret
 
 
-def get_unitary_agate_01(how_many_bits, ctrls, gate):
-    '''
+def get_unitary_agate_01(n, ctrls, gate):
+    """
     get a TOFFOLI like
     # A
     # .
@@ -93,15 +92,15 @@ def get_unitary_agate_01(how_many_bits, ctrls, gate):
     # .
     # .
     # X
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     ret = np.zeros((state_shape, state_shape), dtype=complex)
 
-    if how_many_bits == 2:
+    if n == 2:
         ret = np.kron(E11, gate)
     else:
-        for i in range(2 ** (how_many_bits - 2)):  # 8
-            binary = "{0:>0{1}b}".format(i, how_many_bits - 2)
+        for i in range(2 ** (n - 2)):  # 8
+            binary = "{0:>0{1}b}".format(i, n - 2)
             if i in ctrls:
                 pr = gate
             else:
@@ -121,15 +120,15 @@ def get_unitary_agate_01(how_many_bits, ctrls, gate):
     return ret
 
 
-def get_cnot_ctrl_10(how_many_bits, ctrls, gate):
-    '''
+def get_cnot_ctrl_10(n, ctrls, gate):
+    """
     get a CNOT like
     # X
     # .
     # .
     # C
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     iden = np.identity(state_shape // 4, dtype=complex)
     out = np.outer(zero, np.conjugate(zero).T)
     a = np.kron(np.kron(ide2, iden), out)
@@ -140,13 +139,13 @@ def get_cnot_ctrl_10(how_many_bits, ctrls, gate):
 
 
 def get_cnot_actrl_10(state_shape):
-    '''
+    """
     get a CNOT like
     # X
     # .
     # .
     # A
-    '''
+    """
     iden = np.identity(state_shape // 2, dtype=complex)
     idex = np.identity(state_shape // 4, dtype=complex)
 
@@ -156,8 +155,8 @@ def get_cnot_actrl_10(state_shape):
     return a + b + c
 
 
-def get_unitary_gate_10(how_many_bits, ctrls, gate):
-    '''
+def get_unitary_gate_10(n, ctrls, gate):
+    """
     get a TOFFOLI like
     # X
     # .
@@ -166,8 +165,8 @@ def get_unitary_gate_10(how_many_bits, ctrls, gate):
     # .
     # .
     # C
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     if ctrls[0] - ctrls[1] == 1:
         x = 2 ** ctrls[1]
         r = np.kron(np.identity(x), np.kron(E00, np.identity(2)))
@@ -193,8 +192,8 @@ def get_unitary_gate_10(how_many_bits, ctrls, gate):
     return r
 
 
-def get_unitary_agate_10(how_many_bits, ctrls, gate):
-    '''
+def get_unitary_agate_10(n, ctrls, gate):
+    """
     get a TOFFOLI like
     # X
     # .
@@ -203,8 +202,8 @@ def get_unitary_agate_10(how_many_bits, ctrls, gate):
     # .
     # .
     # A
-    '''
-    state_shape = 2 ** how_many_bits
+    """
+    state_shape = 2 ** n
     if ctrls[0] - ctrls[1] == 1:
         x = 2 ** ctrls[1]
         r = np.kron(np.identity(x), np.kron(E11, np.identity(2)))
@@ -232,9 +231,9 @@ def get_unitary_agate_10(how_many_bits, ctrls, gate):
 
 # SWAP1,3=|0⟩⟨0|⊗I⊗|0⟩⟨0|+E01⊗I⊗E10+E10⊗I⊗E01+|1⟩⟨1|⊗I⊗|1⟩⟨1|
 def get_generic_swap(state_shape):
-    '''
+    """
     get a swap gate
-    '''
+    """
     idex = np.identity(state_shape // 4, dtype=complex)
     out0 = np.outer(zero, np.conjugate(zero).T)
     a = np.kron(np.kron(out0, idex), out0)
